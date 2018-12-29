02757a2f93e67513256b332fdf7c4aa3f18f9d08
theonolev <theo.velon.14@ucl.ac.uk>
2018-12-29 20:05:54 +0100

    Mini proj task A & B
    Tasks A and B are fully completed (might be modified later on) attempt at one of the sub tasks of task C has been made to see if the model can run.
    SVM is running way too slow and the model has not been tuned yet so the accuracy is not good.

--- mini_proj_test.py
+++ mini_proj_test.py
@@ -5,8 +5,10 @@ import numpy as np
 import dlib
 import cv2
 import timeit
+from random import randint
 from skimage.measure import compare_ssim as ssim
 from keras.preprocessing import image
+from sklearn import svm,metrics
 import matplotlib.pyplot as plt
 
 # PATH TO ALL IMAGES
@@ -18,43 +20,46 @@ labels_filename = os.path.join(basedir,'attribute_list.csv')
 image_paths = [os.path.join(images_dir, l) for l in os.listdir(images_dir)]
 target_size = None
 
+starttime = timeit.default_timer()
 def face_detect_gffd(saveimg):
     print("starting gffd detection")
     start = timeit.default_timer()
     outliers_pred = np.array([])
     facedetector = dlib.get_frontal_face_detector()
+
     for imgidx in range(len(saveimg)) :
         facedetected = facedetector(saveimg[imgidx],1)
         if len(facedetected) == 0 :
             outliers_pred = np.append(outliers_pred, imgidx+1)
             # imgfile = str(imgidx + 1) + ".png"
-            # shutil.copy(os.path.join(images_dir, imgfile), os.path.join(basedir, "outliers"))     #used to copy all outliers to another folder
+            # shutil.copy(os.path.join(images_dir, imgfile),os.path.join(basedir, "outliers"))  # used to copy all outliers to another folder
+            # shutil.move(os.path.join(images_dir, imgfile), os.path.join(os.path.join(basedir, "outliers"), imgfile))     #used to move all outliers to another folder
     stop = timeit.default_timer()
     print('Time gffd: ', stop - start)
     return outliers_pred
+
 
 def face_detect_ssim(saveimg,meanimg, thresh = 0.4):
     print("starting ssim detection")
     start = timeit.default_timer()
     ssimarr = np.zeros([len(saveimg)])
     outliers_pred = np.array([])
+
     for imgidx in range(len(saveimg)) :
         ssimarr[imgidx] = ssim(meanimg, saveimg[imgidx], multichannel=False)
         if ssimarr[imgidx] < thresh :
             outliers_pred = np.append(outliers_pred, imgidx + 1)
-            # imgfile = str(imgidx+1)+".png"
-            # shutil.copy(os.path.join(images_dir, imgfile), os.path.join(basedir, "outliers"))
-    # plt.bar(np.arange(1,len(saveimg)+1),ssimarr)
-    # plt.show()
     stop = timeit.default_timer()
     print('Time ssim: ', stop - start)
     return outliers_pred
+
 
 def face_detect_haar(saveimg) :
     print("starting haar detection")
     start = timeit.default_timer()
     face_cascade = cv2.CascadeClassifier('C:\\Users\\TheoV\\PycharmProjects\\untitled\\venv\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
     outliers_pred = np.array([])
+
     for imgidx in range(len(saveimg)):
         facedetected = face_cascade.detectMultiScale(saveimg[imgidx], 1.3, 5)
         if type(facedetected)is tuple :
@@ -63,6 +68,7 @@ def face_detect_haar(saveimg) :
     print('Time haar: ', stop - start)
     return outliers_pred
 
+
 def get_real_outliers(imglabels) :
     outliers_real = np.array([])
     for imgidx in range(len(imglabels)):
@@ -70,21 +76,22 @@ def get_real_outliers(imglabels) :
             outliers_real = np.append(outliers_real, imgidx + 1)
     return outliers_real
 
+
 def get_mean_img(saveimg):
 # get a mean image by taking the mean value of each pixel for all the pictures of non human faces and outliers in the database
     meanimg = np.zeros([256,256])
     for imgidx in range(len(saveimg)) :
         meanimg = np.add(meanimg,saveimg[imgidx-1])
     meanimg = (np.divide(meanimg,len(saveimg))).astype(np.uint8)
-    # plt.imshow(meanimg)
-    # plt.show()
     return meanimg
+
 
 def accuracy_comp(real,pred,labels) :
     truenegarray = [x for x in real if x in pred]
     trueposarray = [x for x in np.arange(1,len(labels)+1) if x not in real and x not in pred]
     falsenegarray = [x for x in pred if x not in real]
     falseposarray = [x for x in real if x not in pred]
+
     print("true negatives = ", len(truenegarray))
     print("false positives = ", len(falseposarray))
     print("false negatives = ", len(falsenegarray))
@@ -93,6 +100,34 @@ def accuracy_comp(real,pred,labels) :
     print("accuracy = ", accur, "%")
     print("\n")
 
+
+def get_datasets(saveimg, imglabels, trainval = 0.8, testval = 0.2, valid = False):
+    shuffledidx = np.arange(len(imglabels))
+    np.random.shuffle(shuffledidx)
+
+    if valid is True :
+        validval = trainval*0.2
+        trainval -= validval
+        validlen = int(len(saveimg)*validval)
+        trainlen = int(len(saveimg) * trainval)
+        testlen = int(len(saveimg) * testval)
+        X_train = saveimg[shuffledidx[:trainlen]]
+        X_valid = saveimg[shuffledidx[np.arange(trainlen,trainlen+validlen)]]
+        X_test = saveimg[shuffledidx[trainlen+validlen:]]
+        Y_train = imglabels[shuffledidx[:trainlen]]
+        Y_valid = imglabels[shuffledidx[np.arange(trainlen, trainlen + validlen)]]
+        Y_test = imglabels[shuffledidx[trainlen + validlen:]]
+        return X_train, X_test, X_valid, Y_train, Y_test, Y_valid
+    else :
+        trainlen = int(len(saveimg) * trainval)
+        testlen = int(len(saveimg) * testval)
+        X_train = saveimg[shuffledidx[:trainlen]]
+        X_test = saveimg[shuffledidx[trainlen:]]
+        Y_train = imglabels[shuffledidx[:trainlen]]
+        Y_test = imglabels[shuffledidx[trainlen:]]
+        return X_train, X_test, Y_train, Y_test
+
+#=======================================================================================================================
 #Open all images and save values
 if os.path.isfile(os.path.join(basedir,"saveimg.npy")) :
     saveimg = np.load(os.path.join(basedir,"saveimg.npy"))
@@ -144,19 +179,58 @@ else :
 
 if not os.path.isdir(os.path.join(basedir, "outliers")):
     os.mkdir(os.path.join(basedir, "outliers"))
+#=======================================================================================================================
 
-meanimg = get_mean_img(saveimg)
+#=======================================================================================================================
+#Task A: face detection to remove outliers
+#tests with different methods for face detection: SSIM, HAAR cascade and HOG => HOG seems most accurate with acc = 95%
+
 outliers_real = np.sort(get_real_outliers(imglabels))
-outliers_pred_haar = np.sort(face_detect_haar(saveimg))
+
 outliers_pred_frontal = np.sort(face_detect_gffd(saveimg))
-outliers_pred_ssim = np.sort(face_detect_ssim(saveimg,meanimg))
+saveimg_gffd = np.delete(saveimg, [x-1 for x in outliers_pred_frontal], axis=0)
+saveimgcolor_gffd = np.delete(saveimgcolor, [x-1 for x in outliers_pred_frontal], axis=0)
+imglabels_gffd = np.delete(imglabels, [x-1 for x in outliers_pred_frontal], axis=0)
+
+# meanimg = get_mean_img(saveimg)
+# outliers_pred_haar = np.sort(face_detect_haar(saveimg))
+# outliers_pred_ssim = np.sort(face_detect_ssim(saveimg,meanimg))
 
 print("\n gffd accuracy:")
 accuracy_comp(outliers_real,outliers_pred_frontal,np.arange(1,len(imglabels)+1))
-print("\n ssim accuracy:")
-accuracy_comp(outliers_real,outliers_pred_ssim,np.arange(1,len(imglabels)+1))
-print("\n haar accuracy:")
-accuracy_comp(outliers_real,outliers_pred_haar,np.arange(1,len(imglabels)+1))
+# print("\n ssim accuracy:")
+# accuracy_comp(outliers_real,outliers_pred_ssim,np.arange(1,len(imglabels)+1))
+# print("\n haar accuracy:")
+# accuracy_comp(outliers_real,outliers_pred_haar,np.arange(1,len(imglabels)+1))
+#=======================================================================================================================
+
+#=======================================================================================================================
+#Task B dataset split to determine training set, testing set and if needed validation set
+X_train, X_test, Y_train, Y_test = get_datasets(saveimg_gffd,imglabels_gffd)
+X_train = np.reshape(X_train,(X_train.shape[0],X_train.shape[1]**2))
+X_test = np.reshape(X_test,(X_test.shape[0],X_test.shape[1]**2))
+
+print("length train set = ", X_train.shape)
+print("length test set = ", X_test.shape)
+print("length train labels = ", Y_train.shape)
+print("length test labels = ", Y_test.shape)
+#=======================================================================================================================
+
+#=======================================================================================================================
+#Task C : 3) Glasses detection
+#test with an SVM
+
+clf_rbf = svm.SVC(gamma='scale')
+clf_rbf.fit(X_train, Y_train[:,1])
+glasses = clf_rbf.predict(X_test)
+accur = metrics.balanced_accuracy_score(Y_test[:,1],glasses)
+confus = metrics.confusion_matrix(Y_test[:,1],glasses)
+print("accuracy = ", accur)             #29/12/2018 => accuracy = 0.5
+print("confusion matrix : \n", confus)
+stoptime = timeit.default_timer()
+print("exec time = ", stoptime-starttime)   #overall exec time = 24 minutes, way too long for one single task
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 
